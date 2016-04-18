8fddf8121e6ea842d9d47dbed5e16c4ccf105dcc
GeorgeErickson <george.erickson.jr@gmail.com>
2016-04-18 13:26:06 -0400

    More docs

--- mrand/mrand.go
+++ mrand/mrand.go
@@ -5,15 +5,17 @@ import (
 	"time"
 )
 
-// Seed the default rand source.
-func Seed() {
+// SeedDefault seeds the default rand source.
+func SeedDefault() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
+// NewSource returns a source that is already seeded.
 func NewSource() rand.Source {
 	return rand.NewSource(time.Now().UnixNano())
 }
 
+// New returns a preseeded rand.Rand
 func New() *rand.Rand {
 	return rand.New(NewSource())
 }
